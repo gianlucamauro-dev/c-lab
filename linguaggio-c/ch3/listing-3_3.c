b3efaebf4a3a2f0fbcf44034f567451692f44d0e
Gianluca Mauro <gianluca.mauro@sandstonelab.com>
2023-11-02 00:27:53 +0100

    Added examples 3.2, 3.3

---
+++ linguaggio-c/ch3/listing-3_3.c
@@ -0,0 +1,16 @@
+// Calcolo area rettanolo, prova utilizzo costanti
+#include <stdio.h>
+
+#define BASE 102
+#define ALTEZZA 34
+
+main()
+{
+    int area;
+
+    area = BASE * ALTEZZA;
+
+    printf("Base: %d\n", BASE);
+    printf("Altezza: %d\n", ALTEZZA);
+    printf("Area: %d\n", area);
+}
